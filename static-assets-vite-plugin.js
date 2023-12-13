c4c83d86f571049ef7bedd47cf3eb25efacfac20
Benjamin Ayles <ben@ayles.com.au>
2023-12-13 15:44:53 +1030

    update to account for dynamic imports

--- static-assets-vite-plugin.js
+++ static-assets-vite-plugin.js
@@ -1,12 +1,26 @@
 function staticAssets() {
     return {
         name: 'static-assets',
-        configResolved(config) {
-            if (process.env.STATIC_ASSETS_BUILD) {
-                config.base = process.env.STATIC_ASSETS_URL;
+        config: () => {
+            // ignore this config if this is not being built by StaticAssets
+            if (! process.env.STATIC_ASSETS_BUILD) {
+                return {};
             }
-        }
-    }
+
+            return {
+                // update all static URLs to use the CDN
+                base: process.env.STATIC_ASSETS_URL,
+                experimental: {
+                    renderBuiltUrl(filename) {
+                        // https://vitejs.dev/guide/build.html#advanced-base-options
+                        // replace all dynamic imports to use the CDN
+
+                        return `${process.env.STATIC_ASSETS_URL}/${filename}`;
+                    }
+                }
+            };
+        },
+    };
 }
 
 module.exports = staticAssets;
